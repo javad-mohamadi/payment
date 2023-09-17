54b7bac72e12ac69c46d09e96fb25b654f72f81f
Javad Mohamadi <javadmohamadi.dev@gmail.com>
2023-09-17 15:02:21 +0330

    fix transaction api

--- app/Criteria/GetUsersTransactionsCriteria.php
+++ app/Criteria/GetUsersTransactionsCriteria.php
@@ -2,7 +2,6 @@
 
 namespace App\Criteria;
 
-use Illuminate\Support\Facades\DB;
 use Prettus\Repository\Contracts\CriteriaInterface;
 use Prettus\Repository\Contracts\RepositoryInterface;
 
@@ -14,9 +13,6 @@ class GetUsersTransactionsCriteria implements CriteriaInterface
 
     public function apply($model, RepositoryInterface $repository)
     {
-        return $model->whereIn('id', $this->ids)
-            ->with(['accounts.transactions' => function ($query) {
-                $query->orderBy('created_at', 'desc');
-            }]);
+        return $model->whereIn('id', $this->ids)->with('accounts.transactions')->get();
     }
 }
--- app/Http/Resources/GetThreeMostTransactionsUsersResource.php
+++ app/Http/Resources/GetThreeMostTransactionsUsersResource.php
@@ -14,6 +14,11 @@ class GetThreeMostTransactionsUsersResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'user_id'      => $this->id,
+            'name'         => $this->name,
+            'mobile'       => $this->mobile,
+            'transactions' => TransactionsResource::collection($this->whenLoaded('accounts'))
+        ];
     }
 }
